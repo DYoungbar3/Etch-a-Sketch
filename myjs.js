aa0ab0fc38b6f53c7bec18556b5d69bfb2afb8d7
Daniel Youngbar <d.youngbar928@gmail.com>
2022-11-19 14:35:25 -0800

    Add permanent color change on mouseover

--- myjs.js
+++ myjs.js
@@ -1,12 +1,25 @@
 const container = document.getElementById('container');
 
-function makeGrid(rows, cols) {
-    for (i = 0; i<(rows*cols); i++) {
+function makeGrid(size) {
+    for (i = 0; i<(size*size); i++) {
         let cell = document.createElement("div");
         cell.classList.add('grid-item')
         cell.innerHTML = '';
         container.appendChild(cell);
+        container.style.gridTemplateColumns = `repeat(${size}, minmax(0, 1fr))`;
     };
 };
 
-makeGrid(16, 16);
+function newGrid() {
+    container.innerHTML = '';
+    let newSize = prompt("Enter desired grid size (<=100)");
+    makeGrid(newSize);
+
+    const cells = document.querySelectorAll('.grid-item');
+    for (const cell of cells) {
+    cell.addEventListener("mouseover", function (){
+        cell.style.backgroundColor = "red";
+    });
+}
+
+}
